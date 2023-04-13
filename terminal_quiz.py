cddb90af304b4fb0cdf253a3c93b97c47ad5d326
BirchAD <andrewbirch1882@icloud.com>
2023-04-13 15:35:25 +0100

    additional docstring

--- terminal_quiz.py
+++ terminal_quiz.py
@@ -9,6 +9,17 @@ score_board = [
     ]
 
 def database_scores():
+    """
+    A function to interact with a SQLite database to store and retrieve user scores.
+
+    Args:
+    None
+
+    Returns:
+    None
+
+    The function establishes a connection to a SQLite database named "user.db" and creates a cursor object to execute SQL queries. It then inserts the scores in the 'score_board' list to the 'user' table using the executemany method of the cursor object. It then retrieves all rows from the 'user' table and prints them to the console. Finally, the function commits the changes and closes the database connection.
+    """
     connection = sqlite3.connect("user.db")
     cursor = connection.cursor()
 
@@ -29,6 +40,7 @@ def get_categories(): #need for front-end.  returns catgory list json
     Returns:
         A list of dictionaries, each containing the ID and name of a trivia category.
     """
+
     cat_url = 'https://opentdb.com/api_category.php'
     cat_response = requests.get(cat_url)
     trivia_categories = cat_response.json()["trivia_categories"]
@@ -42,12 +54,23 @@ def print_categories(categories): # change for front-end to display each categor
     Args:
         categories: A list of dictionaries, each containing the ID and name of a trivia category.
     """
+
     print("Please select a category:")
     for category in categories:
         print(f"{category['id']}: {category['name']}")
 
 
 def get_user_input():
+    """
+    Ask user for input for the following:
+        Category
+        Number of Players
+        Name of Players
+        Difficulty
+    
+    Returns each of the above values
+    """
+    
     user_question_category = int(input("Enter category ID: "))
     num_players = int(input("Enter the number of players: "))
     players = []
@@ -72,7 +95,7 @@ def get_questions(category, difficulty): # returns quiz questions for frontend
 
     url = "https://opentdb.com/api.php"
     params = {
-        "amount": 2,
+        "amount": 10,
         "category": category,
         "difficulty": difficulty,
     }
@@ -85,6 +108,19 @@ def get_questions(category, difficulty): # returns quiz questions for frontend
 
 
 def ask_questions(questions, players):
+    """
+    A function to play a trivia game where players answer questions and earn points based on their correct answers.
+
+    Args:
+    questions (list): A list of dictionaries containing trivia questions and answer options.
+    players (list): A list of strings representing the names of the players.
+
+    Returns:
+    None
+
+    The function iterates through each question in the provided list of questions and prints it to the console along with the answer options. It prompts each player to input their answer for the current question, stores their answer, and updates their score if they answered correctly. Finally, it prints the final scores to the console.
+    """
+
     scores = {player: 0 for player in players}
     player_answers = {player: [] for player in players} # dictionary to store player answers
     for i, question in enumerate(questions):
@@ -96,7 +132,7 @@ def ask_questions(questions, players):
         for j, option in enumerate(answer_options):
             print(f"{j+1}. {option}")
         for player in players:
-            answer = int(input(f"{player}, answer: "))
+            answer = int(input(f"{player}'s, answer: "))
             player_answers[player].append(answer) # store player answer
             if answer == answer_options.index(question['correct_answer'])+1:
                 scores[player] += 1  # add 1 to the current player's score
